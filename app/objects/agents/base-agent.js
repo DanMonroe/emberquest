8dca462a635932f67ab62f75de87e2a8c0d41e22
Dan Monroe <danmonroe67@gmail.com>
2020-03-21 13:05:42 -0400

    Starting to derive stat adjustments based on equipped inventory

--- app/objects/agents/base-agent.js
+++ app/objects/agents/base-agent.js
@@ -5,15 +5,27 @@ import { computed } from '@ember/object';
 export class BaseAgent {
 
   @tracked health;
-  @tracked maxHealth;
-  @tracked healingSpeed;
-  @tracked healingPower;
+  // @tracked maxHealth;
+  // @tracked healingSpeed;
+  // @tracked healingPower;
   @tracked power;
   @tracked maxPower;
   @tracked energizeSpeed = 2000;
   @tracked energizePower = 2;
 
   @tracked inventory = [];
+  @tracked experience = 0;
+
+  // These properties are derived from inventory items from the listed inventory property
+  // attackDamage - from 'damage'
+  // armorHealth - from 'health'
+  // attackSpeedAdj - from 'attackSpeed'
+  // moveSpeedAdj - from 'moveSpeed'
+  // healingSpeedAdj - from 'healingSpeedAdj'
+
+  // maxHealth =  baseHealth + armorHealth
+  // healingSpeed =  baseSpeed + armorHealth
+  // healingPower =  baseHealth + armorHealth
 
   constructor(scene, config) {
     // console.log('in base-agent constructor', scene, config)
@@ -25,20 +37,75 @@ export class BaseAgent {
     this.id = config.id;
 
     this.health = config.health || 10;
-    this.maxHealth = config.maxHealth || 10;
+    // this.maxHealth = config.maxHealth || 10;
     this.power = config.power || 10;
-    this.maxPower = config.maxPower || 10;
-    this.healingSpeed = config.healingSpeed || 10;  // how fast they heal
-    this.healingPower = config.healingPower || 10;  // how much they heal each time
+    this.baseHealingPower = config.baseHealingPower || 2;
+    this.baseHealingSpeed = config.baseHealingSpeed || 2000;  // how fast they heal
+    // this.healingPower = config.healingPower || 10;  // how much they heal each time
     this.energizeSpeed = config.energizeSpeed || 10;// how fast they recharge power
     this.energizePower = config.energizePower || 10;// how much power they recharge each time
 
     this.inventory = config.inventory || [];
   }
 
+  // Main properties:
+
+  get maxHealth() {
+    return this.baseHealth + this.armorHealth;
+  }
+
+  get healingSpeed() {
+    return this.baseHealingSpeed * this.healingSpeedAdj;
+  }
+
+  get healingPower() {
+    return this.baseHealingPower * this.healingSpeedAdj;
+  }
+
+
+  // **************
+
   @computed('inventory.@each.equipped')
   get equippedInventory() {
     return this.inventory.filter(item => item.equipped === true);
+  }
+
+  get level() {
+    if (this.experience < constants.LEVEL_2) { return 1; }
+    if (this.experience < constants.LEVEL_3) { return 2; }
+    if (this.experience < constants.LEVEL_4) { return 3; }
+    if (this.experience < constants.LEVEL_5) { return 4; }
+    if (this.experience < constants.LEVEL_6) { return 5; }
+    if (this.experience < constants.LEVEL_7) { return 6; }
+    if (this.experience < constants.LEVEL_8) { return 7; }
+    if (this.experience < constants.LEVEL_9) { return 8; }
+    if (this.experience < constants.LEVEL_10) { return 9; }
+    if (this.experience < constants.LEVEL_11) { return 10; }
+    if (this.experience < constants.LEVEL_12) { return 11; }
+
+    // after level 11, experience between levels is a constant.
+    return 12 + Math.floor((this.experience - constants.LEVEL_12)/constants.LEVEL_RANGE_AFTER_12);
+  }
+
+  get bonusPercentagePerLevel() {
+    return 1 + (+parseFloat(((constants.HEALTH_BONUS_PER_LEVEL * (this.level-1))) / 10).toFixed(1));
+  }
+
+  get baseHealth() {
+    /**
+     * (20 * level) * (1.2 * level)%
+     */
+    const base = this.level * constants.HEALTH_PER_LEVEL
+    const baseWithBonus = Math.floor(base * this.bonusPercentagePerLevel);
+    // console.log('base, bonus, baseWithBonus', base, bonus, (1 + bonus), baseWithBonus);
+    return baseWithBonus;
+  }
+
+
+  sumProperty(property) {
+    return this.equippedInventory.reduce((sum, item) => {
+      return sum + (item[property] || 0);
+    }, 0);
   }
 
   addInventory(item) {
@@ -54,10 +121,34 @@ export class BaseAgent {
     item.equipped = false;
   }
 
+  get attackDamage() {
+    return this.sumProperty(constants.INVENTORY.DAMAGE);
+  }
+
+  get armorHealth() {
+    return this.sumProperty(constants.INVENTORY.HEALTH);
+  }
+
+  // this will be multiplied by the attackSpeed concurrency timeout
+  // it will add 1.  if all adjustments are 0m the there will be no change to attackSpeed timeout
+  // an adjustment speed less than 1 is faster
+  get attackSpeedAdj() {
+    return +parseFloat((this.sumProperty(constants.INVENTORY.ATTACK_SPEED)).toFixed(constants.FLOATING_POINT_PRECISION)) + 1;
+  }
+  get moveSpeedAdj() {
+    return +parseFloat((this.sumProperty(constants.INVENTORY.MOVE_SPEED)).toFixed(constants.FLOATING_POINT_PRECISION)) + 1;
+  }
+  get healingSpeedAdj() {
+    return +parseFloat((this.sumProperty(constants.INVENTORY.HEALING_SPEED)).toFixed(constants.FLOATING_POINT_PRECISION)) + 1;
+  }
+  get healingPowerAdj() {
+    return +parseFloat((this.sumProperty(constants.INVENTORY.HEALING_POWER)).toFixed(constants.FLOATING_POINT_PRECISION)) + 1;
+  }
+
   getInventoryByType(type, equippedOnly = true) {
     switch (type) {
-      case constants.TYPE_ARMOR:
-      case constants.TYPE_WEAPON:
+      case constants.INVENTORY.TYPE.ARMOR:
+      case constants.INVENTORY.TYPE.WEAPON:
         return equippedOnly ? this.equippedInventory.filter(item => item.type === type) : this.inventory.filter(item => item.type === type);
       default:
         return [];
@@ -66,21 +157,21 @@ export class BaseAgent {
 
   getInventoryByResistance(type) {
     switch (type) {
-      case constants.RESISTANCE_FIRE:
+      case constants.INVENTORY.RESISTANCE.FIRE:
         return this.equippedInventory.filter(item => {
           if (!item.resistance || item.resistance.length === 0) {
             return false;
 
           }
-          return item.resistance.some(resistance => resistance.type === constants.RESISTANCE_FIRE);
+          return item.resistance.some(resistance => resistance.type === constants.INVENTORY.RESISTANCE.FIRE);
         });
-      case constants.RESISTANCE_COLD:
+      case constants.INVENTORY.RESISTANCE.COLD:
         return this.equippedInventory.filter(item => {
           if (!item.resistance || item.resistance.length === 0) {
             return false;
 
           }
-          return item.resistance.some(resistance => resistance.type === constants.RESISTANCE_COLD);
+          return item.resistance.some(resistance => resistance.type === constants.INVENTORY.RESISTANCE.COLD);
         });
       default:
         return [];
@@ -90,8 +181,8 @@ export class BaseAgent {
   getResistance(type) {
     let totalResistance = 0;
     switch (type) {
-      case constants.RESISTANCE_FIRE:
-      case constants.RESISTANCE_COLD:
+      case constants.INVENTORY.RESISTANCE.FIRE:
+      case constants.INVENTORY.RESISTANCE.COLD:
 
         // sum all inventory items, and each resistance object in that item.
         totalResistance += this.getInventoryByResistance(type).reduce((sum, { resistance } ) => {
--- tests/unit/objects/base-agent-test.js
+++ tests/unit/objects/base-agent-test.js
@@ -1,9 +1,8 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-// import { setupMirage } from 'ember-cli-mirage/test-support';
 import { BaseAgent } from 'emberquest/objects/agents/base-agent';
-import { constants } from 'emberquest/services/constant';
 import { InventoryItem } from 'emberquest/objects/models/inventory-item';
+import { constants } from 'emberquest/services/constant';
 
 const scene = {
   board: {},
@@ -15,13 +14,140 @@ let armor1;
 let armor2;
 let armor3;
 let weapon1;
+let weapon2;
+let ring1;
 
 module('Unit | Object | base-agent', function(hooks) {
   setupTest(hooks);
-  // setupMirage(hooks);
 
   hooks.beforeEach(function() {
     agent = new BaseAgent(scene, {});
+
+    armor1 = new InventoryItem({
+      type: constants.INVENTORY.TYPE.ARMOR,
+      name: 'armor1',
+      equipped: true,
+      health: 10,
+      resistance: [{
+        type: constants.INVENTORY.RESISTANCE.FIRE,
+        value: 7
+      }]
+    });
+    armor2 = new InventoryItem({
+      type: constants.INVENTORY.TYPE.ARMOR,
+      name: 'armor2',
+      equipped: true,
+      health: 3,
+      resistance: [{
+        type: constants.INVENTORY.RESISTANCE.FIRE,
+        value: 95
+      }]
+    });
+    armor3 = new InventoryItem({
+      type: constants.INVENTORY.TYPE.ARMOR,
+      name: 'armor3',
+      equipped: true,
+      health: 5,
+      resistance: [
+        {
+          type: constants.INVENTORY.RESISTANCE.FIRE,
+          value: 2
+        },
+        {
+          type: constants.INVENTORY.RESISTANCE.COLD,
+          value: 42
+        }
+      ]
+    });
+    weapon1 = new InventoryItem({
+      type: constants.INVENTORY.TYPE.WEAPON,
+      name: 'weapon1',
+      equipped: true,
+      damage: 6,
+      resistance: [{
+        type: constants.INVENTORY.RESISTANCE.FIRE,
+        value: 3
+      }]
+    });
+    weapon2 = new InventoryItem({
+      type: constants.INVENTORY.TYPE.WEAPON,
+      name: 'weapon2',
+      equipped: true,
+      damage: 7,
+      moveSpeed: 0.5,
+      attackSpeed: 1.1, // really big so it is slow
+      resistance: [{
+        type: constants.INVENTORY.RESISTANCE.FIRE,
+        value: 3
+      }]
+    });
+    ring1 = new InventoryItem({
+      name: 'ring1',
+      equipped: true,
+      attackSpeed: -0.2,
+      healingSpeedAdj: -0.5
+    });
+
+  });
+
+  module('Levels', function(hooks) {
+    test('level 1', function(assert) {
+      assert.equal(agent.level, 1, "initial level");
+    });
+    test('levels based on experience', function(assert) {
+      agent.experience = constants.LEVEL_1;
+      assert.equal(agent.level, 1, 'initial level');
+      assert.equal(agent.baseHealth, 20, 'initial health');
+
+      agent.experience = constants.LEVEL_2;
+      assert.equal(agent.level, 2, 'level 2');
+      assert.equal(agent.baseHealth, 44, 'level 2');
+
+      agent.experience = constants.LEVEL_3;
+      assert.equal(agent.level, 3, 'level 3');
+      assert.equal(agent.baseHealth, 72, 'level 3');
+
+      agent.experience = constants.LEVEL_4;
+      assert.equal(agent.level, 4, 'level 4');
+
+      agent.experience = constants.LEVEL_5;
+      assert.equal(agent.level, 5, 'level 5');
+
+      agent.experience = constants.LEVEL_6;
+      assert.equal(agent.level, 6, 'level 6');
+
+      agent.experience = constants.LEVEL_7;
+      assert.equal(agent.level, 7, 'level 7');
+
+      agent.experience = constants.LEVEL_8;
+      assert.equal(agent.level, 8, 'level 8');
+
+      agent.experience = constants.LEVEL_9;
+      assert.equal(agent.level, 9, 'level 9');
+
+      agent.experience = constants.LEVEL_10;
+      assert.equal(agent.level, 10, 'level 10');
+
+      agent.experience = constants.LEVEL_11;
+      assert.equal(agent.level, 11, 'level 11');
+
+      agent.experience = constants.LEVEL_12;
+      assert.equal(agent.level, 12, 'level 12');
+
+      agent.experience = constants.LEVEL_12 + 1;
+      assert.equal(agent.level, 12, 'level 12 - barely');
+
+      agent.experience = constants.LEVEL_12 + constants.LEVEL_RANGE_AFTER_12;
+      assert.equal(agent.level, 13, 'level 13');
+
+      agent.experience = constants.LEVEL_12 + constants.LEVEL_RANGE_AFTER_12 + 1;
+      assert.equal(agent.level, 13, 'level 13');
+      // console.log(agent.baseHealth);
+
+      agent.experience = constants.LEVEL_12 + (constants.LEVEL_RANGE_AFTER_12 * 11);
+      assert.equal(agent.level, 23, 'level 23');
+      // console.log(agent.baseHealth);
+    });
   });
 
   test('Base inventory test', function(assert) {
@@ -29,51 +155,69 @@ module('Unit | Object | base-agent', function(hooks) {
     assert.equal(agent.inventory.length, 0, 'Empty Inventory');
   });
 
-  module('Add to inventory with some fire resistance', function (hooks) {
-    hooks.beforeEach(function() {
-      armor1 = new InventoryItem({
-        type: constants.TYPE_ARMOR,
-        name: 'armor1',
-        equipped: false,
-        resistance: [{
-          type: constants.RESISTANCE_FIRE,
-          value: 7
-        }]
-      });
-      armor2 = new InventoryItem({
-        type: constants.TYPE_ARMOR,
-        name: 'armor2',
-        equipped: false,
-        resistance: [{
-          type: constants.RESISTANCE_FIRE,
-          value: 95
-        }]
-      });
-      armor3 = new InventoryItem({
-        type: constants.TYPE_ARMOR,
-        name: 'armor3',
-        equipped: false,
-        resistance: [
-          {
-            type: constants.RESISTANCE_FIRE,
-            value: 2
-          },
-          {
-            type: constants.RESISTANCE_COLD,
-            value: 42
-          }
-        ]
-      });
-      weapon1 = new InventoryItem({
-        type: constants.TYPE_WEAPON,
-        name: 'weapon1',
-        equipped: false,
-        resistance: [{
-          type: constants.RESISTANCE_FIRE,
-          value: 3
-        }]
-      });
+  module('Speed Adjustments', function() {
+    test('attack speed for one item', function (assert) {
+
+      agent.addInventory(ring1);
+
+      assert.equal(agent.attackSpeedAdj, 0.8, 'attack Speed Adj');
     });
+    test('attack speed adjustment is cumulative', function (assert) {
+
+      agent.addInventory(ring1);  // -0.2
+      agent.addInventory(weapon2);// 1.1
+
+      assert.equal(agent.attackSpeedAdj, 1.9, 'attack Speed Adj - two items');
+    });
+    test('multiple adjustments', function (assert) {
+
+      agent.addInventory(ring1);
+      agent.addInventory(weapon2);
+
+      assert.equal(agent.attackSpeedAdj, 1.9, 'attack Speed Adj');
+      assert.equal(agent.moveSpeedAdj, 1.5, 'move Speed Adj');
+      assert.equal(agent.healingSpeedAdj, 0.5, 'healing Speed Adj');
+    });
+  });
+
+  module('Armor Health', function() {
+    test('armor has additional health', function (assert) {
+
+      agent.addInventory(armor1);
+
+      assert.equal(agent.armorHealth, 10, 'armor health');
+    });
+    test('armorHealth and baseHealth combined for maxHealth', function (assert) {
+
+      agent.addInventory(armor1);
+
+      assert.equal(agent.armorHealth, 10, 'armor health');
+
+      agent.experience = constants.LEVEL_3;
+      assert.equal(agent.level, 3, 'level 3');
+      assert.equal(agent.baseHealth, 72, 'baseHealth');
+
+      assert.equal(agent.maxHealth, 82, 'maxHealth');
+
+    });
+  });
+
+  module('Attack Damage', function() {
+    test('weapon has damage potential', function (assert) {
+      agent.addInventory(weapon1);
+
+      assert.equal(agent.attackDamage, 6, 'Attack damage');
+    });
+    test('two weapons add damage potential', function (assert) {
+      agent.addInventory(weapon1);
+      agent.addInventory(weapon2);
+
+      assert.equal(agent.attackDamage, 13, 'Collective Attack damage');
+    });
+  });
+
+  module('Add to inventory with some fire resistance', function (hooks) {
+
 
     test('Add to inventory', function(assert) {
 
@@ -82,33 +226,39 @@ module('Unit | Object | base-agent', function(hooks) {
       assert.equal(agent.inventory.length, 1);
       const firstItem = agent.inventory[0];
       assert.equal(firstItem.name, 'armor1');
-      assert.equal(firstItem.type, constants.TYPE_ARMOR);
+      assert.equal(firstItem.type, constants.INVENTORY.TYPE.ARMOR);
     });
 
     test('agent has 1 armor and 1 weapon', function(assert) {
 
+      agent.unequipItem(armor1);
+      agent.unequipItem(weapon1);
+
       agent.addInventory(armor1);
       agent.addInventory(weapon1);
 
-      assert.equal(agent.getInventoryByType(constants.TYPE_ARMOR, false).length, 1, 'Armor type');
-      assert.equal(agent.getInventoryByType(constants.TYPE_WEAPON, false).length, 1, 'Weapon type');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.ARMOR, false).length, 1, 'Armor type');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.WEAPON, false).length, 1, 'Weapon type');
 
-      assert.equal(agent.getInventoryByType(constants.TYPE_ARMOR).length, 0, 'no Armor equipped');
-      assert.equal(agent.getInventoryByType(constants.TYPE_WEAPON,).length, 0, 'no weapons equipped');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.ARMOR).length, 0, 'no Armor equipped');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.WEAPON,).length, 0, 'no weapons equipped');
 
       agent.equipItem(armor1);
       agent.equipItem(weapon1);
 
-      assert.equal(agent.getInventoryByType(constants.TYPE_ARMOR).length, 1, '1 Armor equipped');
-      assert.equal(agent.getInventoryByType(constants.TYPE_WEAPON,).length, 1, '1 weapon equipped');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.ARMOR).length, 1, '1 Armor equipped');
+      assert.equal(agent.getInventoryByType(constants.INVENTORY.TYPE.WEAPON,).length, 1, '1 weapon equipped');
     });
 
     test('agent has some fire resistance; some from armor and some from weapons, but none equipped', function(assert) {
 
+      agent.unequipItem(armor1);
+      agent.unequipItem(weapon1);
+
       agent.addInventory(armor1);
       agent.addInventory(weapon1);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 0);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 0);
     });
 
     test('agent has some fire resistance; some from armor and some from weapons', function(assert) {
@@ -119,7 +269,7 @@ module('Unit | Object | base-agent', function(hooks) {
       agent.equipItem(armor1);
       agent.equipItem(weapon1);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 10);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 10);
     });
 
     test('cant have over 100 percent resistance', function(assert) {
@@ -132,7 +282,7 @@ module('Unit | Object | base-agent', function(hooks) {
       agent.equipItem(armor2);
       agent.equipItem(weapon1);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 100);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 100);
     });
 
     test('agent has some fire resistance and some cold', function(assert) {
@@ -145,16 +295,16 @@ module('Unit | Object | base-agent', function(hooks) {
       agent.equipItem(armor3);
       agent.equipItem(weapon1);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 12, 'fire');
-      assert.equal(agent.getResistance(constants.RESISTANCE_COLD), 42, 'cold');
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 12, 'fire');
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.COLD), 42, 'cold');
     });
     test('agent has some fire resistance and some cold from same item', function(assert) {
 
       agent.addInventory(armor3);
       agent.equipItem(armor3);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 2);
-      assert.equal(agent.getResistance(constants.RESISTANCE_COLD), 42);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 2);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.COLD), 42);
     });
 
     test('unequip reduces resistance', function(assert) {
@@ -164,13 +314,13 @@ module('Unit | Object | base-agent', function(hooks) {
       agent.equipItem(armor1);
       agent.equipItem(armor3);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 9);
-      assert.equal(agent.getResistance(constants.RESISTANCE_COLD), 42);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 9);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.COLD), 42);
 
       agent.unequipItem(armor1);
 
-      assert.equal(agent.getResistance(constants.RESISTANCE_FIRE), 2);
-      assert.equal(agent.getResistance(constants.RESISTANCE_COLD), 42);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.FIRE), 2);
+      assert.equal(agent.getResistance(constants.INVENTORY.RESISTANCE.COLD), 42);
 
     });
   });
